e7a124bb8840ef9af2cac291679a0c1a108112d3
Daniel Santos Pereira Lira <danielsplira@gmail.com>
2023-02-16 10:29:18 -0300

    Create exercicio7.php

---
+++ exercicio7.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 07 PHP</title>
+</head>
+
+<body>
+    <form action="" method="get">
+        <label>Que turno você estuda?</label>
+        <select id="periodo">
+            <option value="m">M</option>
+            <option value="v">V</option>
+            <option value="n">N</option>
+        </select>
+    </form>
+
+</body>
+
+</html>
