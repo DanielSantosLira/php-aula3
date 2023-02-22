8e25b39983d2fa5dc23e3b8d1ce4eefff37bfa11
Daniel Santos Pereira Lira <danielsplira@gmail.com>
2023-02-22 19:45:03 -0300

    Update exercicio8.php

--- exercicio8.php
+++ exercicio8.php
@@ -1,25 +1,32 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercicio 8 PHP</title>
 </head>
+
 <body>
-<h2>conforme o caso.
+    <h2>conforme o caso.
         As Organizações Tabajara resolveram dar um aumento de salário aos seus <br>
         colaboradores e lhe contraram para desenvolver o programa que calculará os reajustes</h2><br>
     <form action="" method="get">
-    <label for="">Reajuste de Salario</label>
-     <input type="text" value="" id="txtvalue" name="salario" />
-    <button type="submit">Calcular</button>
+        <label for="">Reajuste de Salario</label>
+        <input type="text" value="" id="txtvalue" name="salario" />
+        <button type="submit">Calcular</button>
     </form>
 
     <?php
 
-$salario = $_GET['salario']
+    $salario = $_GET['salario'];
+
+    if(){
+        
+    }
 
     ?>
 </body>
+
 </html>
