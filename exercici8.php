fceca4a03a17e8e760656de0c21f81ca6965c046
Daniel Santos Pereira Lira <danielsplira@gmail.com>
2023-02-22 10:16:10 -0300

    Create exercici8.php

---
+++ exercici8.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 8 PHP</title>
+</head>
+<body>
+    <form action="" method="get">
+    <label for="">Reajuste de Salario</label>
+     <input type="text" value="" id="txtvalue" name="txtvalue" />
+    <button type="submit">Calcular</button>
+    </form>
+
+    <?php
+
+$salario = $_GET['']
+
+    ?>
+</body>
+</html>
