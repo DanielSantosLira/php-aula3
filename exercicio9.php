12097ff69a6960fb7f1c5b2aaa6cad436a68cd1a
Daniel Santos Pereira Lira <danielsplira@gmail.com>
2023-03-23 19:27:01 -0300

    Create exercicio9.php

---
+++ exercicio9.php
@@ -0,0 +1,25 @@
+<?php
+/* Problema: verificar se a idade da
+pessoa é maior ou igual de 18 anos,
+se for mostrar a mensagem "PODE DIRIGIR"
+senão nãp mostar nada
+ */
+
+$idade = 20;
+if($idade >= 18){
+    echo "pode dirigir";
+}
+echo"<hr>";
+/* Problema: verificar se a idade da
+pessoa é maior ou igual de 18 anos,
+se for mostrar a mensagem "PODE DIRIGIR"
+senão não mostar mensagem "pode dirigir"
+ */
+$idade = 18;
+ if($idae >= 18){
+    echo "pode dirigir";
+ }else{
+    echo "não pode dirigir";
+ }
+ 
+?>
