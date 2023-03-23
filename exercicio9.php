485f4274e9fda8347c0a14d93d164b6a0ca90ee1
Daniel Santos Pereira Lira <danielsplira@gmail.com>
2023-03-23 20:06:21 -0300

    Update exercicio9.php

--- exercicio9.php
+++ exercicio9.php
@@ -10,19 +10,43 @@ if($idade >= 18){
     echo "pode dirigir";
 }
 echo"<hr>";
+
 /* Problema: verificar se a idade da
 pessoa é maior ou igual de 18 anos,
 se for mostrar a mensagem "PODE DIRIGIR"
-senão não mostar mensagem "pode dirigir"
+senão não mostar mensagem "menor de idade"
  */
-$idade = 18;
+$idade = 15;
 
  if($idade >= 18){
     echo "pode dirigir";
  }else{
-    echo "não pode dirigir";
+    echo "Menor de idade";
    
  }
- 
- 
-?>
+
+echo "<hr>";
+
+/* Problema: verificar se a idade da
+pessoa é maior ou igual de 18 anos,
+se for mostrar a mensagem "PODE DIRIGIR"
+senão verificar se a idade é maior ou igual á
+90 anos se for, mostar a mensagem "já paoou do tempo , não
+pode mais dirigir, senão mostar "manor de idade"
+ */
+
+ $idade = 15;
+
+ if($idade >=90){
+   echo "Já passou do tempo, não pode mais dirigir";
+
+ }else if($idade >= 18){
+   echo "Pode dirigir";
+ }else{
+    echo "Maior de idade";
+ }
+
+ echo  "<hr>";
+
+ /* Problema: verificar se idade da pessoas 
+  */
