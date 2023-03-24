c41a40604efaba499d0cff1a073cd8671c371ca4
Daniel Santos Pereira Lira <danielsplira@gmail.com>
2023-03-23 21:19:37 -0300

    Update exercicio9.php

--- exercicio9.php
+++ exercicio9.php
@@ -5,25 +5,28 @@ se for mostrar a mensagem "PODE DIRIGIR"
 senão nãp mostar nada
  */
 
-$idade = 20;
-if($idade >= 18){
-    echo "pode dirigir";
+$idade = 19;
+
+if ($idade >= 18) {
+   echo "Pode Dirigir";
 }
-echo"<hr>";
+
+echo "<hr>";
 
 /* Problema: verificar se a idade da
 pessoa é maior ou igual de 18 anos,
 se for mostrar a mensagem "PODE DIRIGIR"
 senão não mostar mensagem "menor de idade"
  */
+
+
 $idade = 15;
 
- if($idade >= 18){
-    echo "pode dirigir";
- }else{
-    echo "Menor de idade";
-   
- }
+if ($idade >= 18) {
+   echo "Pode Dirigir";
+} else {
+   echo "Menor de Idade";
+}
 
 echo "<hr>";
 
@@ -35,18 +38,108 @@ senão verificar se a idade é maior ou igual á
 pode mais dirigir, senão mostar "manor de idade"
  */
 
- $idade = 15;
+$idade = 15;
 
- if($idade >=90){
+if ($idade >= 90) {
    echo "Já passou do tempo, não pode mais dirigir";
+} else if ($idade >= 18) {
+   echo "Pode Dirigir";
+} else {
+   echo "Maior de Idade";
+}
 
- }else if($idade >= 18){
-   echo "Pode dirigir";
- }else{
-    echo "Maior de idade";
- }
+echo  "<hr>";
 
- echo  "<hr>";
+/*PROBLEMA: verificar se a idade da pessoa é maior ou igual de 18 anos, se 
+ for mostrar a mensagem "pode dirigir" senão mostrar "menor de idade" e a 
+ quantidade de anos que falta para a pessoa ser maior de idade */
 
- /* Problema: verificar se idade da pessoas 
-  */
+
+$idade = 10;
+if ($idade >= 18) {
+   echo "Pode Dirigir";
+} else {
+   $diferenca = 18 - $idade;
+   echo "Maior de idade, volte em $diferenca anos";
+}
+echo "<hr>";
+
+/*
+PROBLEMA: crie um código que verifique a cor informada pelo usuário e
+ gere um código hexadecimal para formatar a cor do texto. As cores 
+ permitidas são: azul, vermelho e verde. Caso seja diferente, deixar 
+ a letra preta;
+*/
+
+$corEscolhida = "verde";
+$corHexa = "";
+
+switch ($corEscolhida) {
+   case "vermelho":
+      $corHexa = "#ff0000";
+      break;
+   case "azul":
+      $corHexa = "#0000ff";
+      break;
+   case "verde":
+      $corHexa = "#00ff00";
+      break;
+   default:
+      $corHexa = "#000000";
+      break;
+}
+
+echo "<h1 style='color:$corHexa'> Daniel é  muito lindo</h1>";
+
+echo "<hr>";
+
+/*
+PROBLEMA: exiba os números de 0 à 20
+*/
+
+$contador = 0;
+
+while ($contador <= 20) {
+   echo $contador . "<br>";
+   //$contador = $contador + 1
+   $contador++;
+}
+
+echo "<hr>";
+
+/*
+PROBLEMA: exiba os números de 10 à 0
+*/
+
+$contador = 10;
+while ($contador >= 0) {
+   echo "$contador <br>";
+   $contador--;
+}
+
+echo "<hr>";
+
+for($i = 0 ; $i <= 20; $i++){
+   echo "$i <br>";
+
+}
+
+echo "<hr>";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
