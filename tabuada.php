3b090af5f2fedc91b141a3ec0619b681a3b053a5
Daniel Santos Pereira Lira <danielsplira@gmail.com>
2023-03-30 19:37:42 -0300

    Create tabuada.php

---
+++ tabuada.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tabuada</title>
+</head>
+<body>
+    <h1>Tabuada do 8 </h1>
+<?php
+ echo "<h3> 8*1 = ".(8*1)."</h3>";
+ echo "<h3> 8*2 = ".(8*2)."</h3>";
+ echo "<h3> 8*3 = ".(8*3)."</h3>";
+ echo "<h3> 8*4 = ".(8*4)."</h3>";
+ echo "<h3> 8*5 = ".(8*5)."</h3>";
+ echo "<h3> 8*6 = ".(8*6)."</h3>";
+ echo "<h3> 8*7 = ".(8*7)."</h3>";
+ echo "<h3> 8*8 = ".(8*8)."</h3>";
+ echo "<h3> 8*9 = ".(8*9)."</h3>";
+ echo "<h3> 8*10 = ".(8*10)."</h3>";
+
+
+?>
+</body>
+</html>
