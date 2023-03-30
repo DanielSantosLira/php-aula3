4531ea30cf1a0d1cf1b7ab8ee939a40566b10dac
Daniel Santos Pereira Lira <danielsplira@gmail.com>
2023-03-29 22:10:16 -0300

    Update salvar-cliente.php

--- salvar-cliente.php
+++ salvar-cliente.php
@@ -1,40 +1,59 @@
 <?php
- $nome = $_POST['nome'] ?? "";
- $email = $_POST['email'] ?? "";
- $telefone = $_POST['tel'] ?? "";
- $rua = $_POST['rua'] ?? "";
- $bairro = $_POST['bairro'] ?? "";
- $complemento = $_POST['comple'] ?? "";
- $cidade = $_POST['cid'] ?? "";
- $estado= $_POST['est'] ?? "";
- $datadenacimento = $_POST['dtn'] ?? "";
-$cpf = $_POST['cpf']?? "";
-$rg = $_POST['rg']?? "";
-$whatsapp  = $_POST["whats"] ?? "";
-$babida  = $_POST["bebida"] ?? "";
 
-echo $_POST['nome']  ;
-echo "<br>";
-echo $_POST['email'];
-echo "<br>";
-echo $_POST['tel'];
-echo $_POST['rua']  ;
-echo "<br>";
-echo $_POST['bairro'];
-echo "<br>";
-echo $_POST['comple'];
-echo $_POST['cid']  ;
-echo "<br>";
-echo $_POST['est'];
-echo "<br>";
-echo $_POST['dtn'];
-echo $_POST['cpf']  ;
-echo "<br>";
-echo $_POST['rg'];
-echo "<br>";
-echo $_POST['whats'];
-echo "<br>";
-echo $_POST['bebida'];
+ $nome = $_POST["nome"] ?? "";
+
+ $email = $_POST['email'] ?? "";
+
+ $telefone = $_POST['telefone'] ?? "";
+
+ $rua = $_POST['rua'] ?? "";
+
+ $bairro = $_POST['bairro'] ?? "";
+
+ $complemento = $_POST['complemento'] ?? "";
+
+ $cidade = $_POST['cidade'] ?? "";
+
+ $estado= $_POST['estado'] ?? "";
+
+ $datadenacimento = $_POST['datadenacimento'] ?? "";
+
+$cpf = $_POST['cpf']?? "";
+
+$rg = $_POST['rg']?? "";
+
+$whatsapp  = $_POST["whats"] ?? "";
+
+$bebida  = $_POST["bebida"] ?? "";
+
+
+echo "Nome: $nome <br>";
+
+echo "E-mail: $email <br>";
+
+echo "Telefone: $telefone <br>";
+
+echo "Rua: $rua <br>";
+
+echo "Bairro: $bairro <br>";
+
+echo "Complemento: $complemento <br>";
+
+echo "Cidade: $cidade <br>";
+
+echo "Estado: $estado <br>";
+
+echo "Data de Nacimento: $datadenacimento <br>";
+
+echo "CPG: $cpf <br>";
+
+echo "RG: $rg <br>";
+
+echo "Whastsapp: $whatsapp <br>";
+
+echo "Bebida: $bebida <br>";
+
+
 
 
 ?>
