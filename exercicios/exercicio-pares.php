d331a2ea44a7baf7e3f6a999e3224622ccdba8a3
Daniel Santos Pereira Lira <danielsplira@gmail.com>
2023-03-30 22:21:15 -0300

    Create exercicio-pares.php

---
+++ exercicios/exercicio-pares.php
@@ -0,0 +1,30 @@
+<!Doctype html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Exercicio 01 Número par</title>
+
+</head>
+
+<body>
+
+    <?php
+
+    $numero = 0;
+
+
+    while ($numero <= 100) {
+
+        echo $numero . "<br>";
+        $numero = $numero + 2;
+    }
+
+    ?>
+
+</body>
+
+
+
+</html>
