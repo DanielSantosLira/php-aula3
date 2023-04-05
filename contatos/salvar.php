018b73b476c492314769b4552f31f41e8c86d799
Daniel Santos Pereira Lira <danielsplira@gmail.com>
2023-04-05 19:47:37 -0300

    Create salvar.php

---
+++ contatos/salvar.php
@@ -0,0 +1,12 @@
+<?php
+
+$nome = $_POST['nome'];
+$cidade = $_POST['cidade'];
+$telefone = $_POST['telefone'];
+
+$arquivo = fopen('clientes.csv', 'a');
+ fwrite($arquivo, $nome . ",");
+ fwrite($arquivo, $cidade . ",");
+ fwrite($arquivo, $telefone . "\n");
+
+?>
