fb05114f4f9d06d3fb630e9e98e2550def746211
Daniel Santos Pereira Lira <danielsplira@gmail.com>
2023-04-05 20:31:09 -0300

    Update salvar.php

--- contatos/salvar.php
+++ contatos/salvar.php
@@ -2,11 +2,14 @@
 
 $nome = $_POST['nome'];
 $cidade = $_POST['cidade'];
+$estado = $_REQUEST['estado'];
 $telefone = $_POST['telefone'];
 
 $arquivo = fopen('clientes.csv', 'a');
  fwrite($arquivo, $nome . ",");
  fwrite($arquivo, $cidade . ",");
+ fwrite($arquivo, $estado . ",");
+
  fwrite($arquivo, $telefone . "\n");
 
 ?>
